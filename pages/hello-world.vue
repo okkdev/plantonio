c7ba6a849b4de9d3642f8993b8bd5e49fb5d8539
Ruslan Steiger <steiger.ruslan@gmail.com>
2019-11-02 13:33:24 +0100

    hello world

---
+++ pages/hello-world.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-full flex items-center justify-center">
+    <button @click="hello">🔥</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    hello() {
+      console.log('🎁')
+    }
+  }
+}
+</script>
