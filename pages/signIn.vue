e18b19ee5c1a7726a2dc39763eedfac1e5ea70df
hactar <hactar@cyberkraft.org>
2019-11-02 21:31:49 +0100

    Fixes

--- pages/signIn.vue
+++ pages/signIn.vue
@@ -2,22 +2,24 @@
   <div>
     <div class="flex justify-center w-full">
       <form class="flex flex-col w-2/3">
-        <label class="block mb-2"
-          ><span class=" font-semibold uppercase block">Email</span
-          ><input
+        <label class="block mb-2">
+          <span class=" font-semibold uppercase block">Email</span>
+          <input
             required
             class="form-input w-full block"
             type="email"
             placeholder="E-Mail"
-        /></label>
-        <label class="block mb-2"
-          ><span class=" font-semibold uppercase block">Password</span
-          ><input
+          />
+        </label>
+        <label class="block mb-2">
+          <span class=" font-semibold uppercase block">Password</span>
+          <input
             required
             class="form-input w-full block"
             type="password"
             placeholder="Password"
-        /></label>
+          />
+        </label>
         <div class="flex flex-col items-center justify-center mt-3">
           <Button title="Sign In" link="/overview" />
         </div>
