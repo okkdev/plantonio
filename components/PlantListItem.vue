4c98df3985f626a983de514cf2d129eb531a99c0
hactar <hactar@cyberkraft.org>
2019-11-03 10:00:33 +0100

    Pages reorganization

---
+++ components/PlantListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <nuxt-link
+    tag="li"
+    :to="'/plants/' + plant.id"
+    class="flex group hover:bg-teal-400 rounded p-2 m-4 mx-2 bg-white shadow-lg items-center"
+  >
+    <figure class="object-fill w-20 h-20 mr-3 rounded">
+      <img :src="plant.img" class="h-full w-full" />
+    </figure>
+    <dl class="flex-grow group-hover:text-teal-100">
+      <div class="flex">
+        <dt class="mr-2 font-semibold">Name:</dt>
+        <dd>{{ plant.name }}</dd>
+      </div>
+      <div class="flex">
+        <dt class="mr-2 font-semibold">Nic:</dt>
+        <dd>{{ plant.nic }}</dd>
+      </div>
+    </dl>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    plant: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
--- pages/overview.vue
+++ pages/overview.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <CardLink title="Search Plant" img="/img/mobile-image.svg" />
-    <CardLink title="My Plants" link="/myPlants" img="/img/my-plant.svg" />
+    <CardLink title="My Plants" link="/plants" img="/img/my-plant.svg" />
     <CardLink title="My Progress" img="/img/progress.svg" />
-    <Nav />
   </div>
 </template>
 
